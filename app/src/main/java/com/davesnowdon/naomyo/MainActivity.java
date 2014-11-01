c1ff3cb2d762807128f563d80171362922e50dba
Dave Snowdon <dave@davesnowdon.com>
2014-11-01 06:17:39 +0000

    Got connection to NAO working

--- app/src/main/java/com/davesnowdon/naomyo/MainActivity.java
+++ app/src/main/java/com/davesnowdon/naomyo/MainActivity.java
@@ -1,17 +1,37 @@
 package com.davesnowdon.naomyo;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.aldebaran.qimessaging.Session;
+import com.aldebaran.qimessaging.helpers.al.ALMemory;
+import com.aldebaran.qimessaging.helpers.al.ALMotion;
+import com.aldebaran.qimessaging.helpers.al.ALTextToSpeech;
+
+import java.util.concurrent.TimeUnit;
 
 
 public class MainActivity extends Activity {
+    private static  final String TAG = "MainActivity";
+    private Session naoSession;
+    private ALMotion motionProxy;
+    private ALMemory memoryProxy;
+    private ALTextToSpeech ttsProxy;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
     }
 
 
@@ -32,5 +52,50 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onClickConnect(View view) {
+        Log.i(TAG, "Connect clicked");
+        EditText ipView = (EditText) findViewById(R.id.robot_ip_edit);
+        final String addr = ipView.getText().toString();
+        Log.i(TAG, "Robot IP text field: " + addr);
+
+        if (isNotBlank(addr)) {
+            doConnect(addr);
+        } else {
+            Log.i(TAG, "Robot address was blank");
+            Toast.makeText(context, "Please enter a robot ip or a robot name", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void doConnect(final String addr) {
+        if (isNotBlank(addr)) {
+            Log.i(TAG, "Robot address: " + addr);
+
+            Thread connectThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    naoSession = new Session();
+                    try {
+                        naoSession.connect("tcp://" + addr + ":9559").sync(500, TimeUnit.MILLISECONDS);
+
+                        motionProxy = new ALMotion(naoSession);
+                        ttsProxy = new ALTextToSpeech(naoSession);
+                        ttsProxy.setAsynchronous(true);
+                        memoryProxy = new ALMemory(naoSession);
+                        Log.i(TAG, "Connected to "+addr);
+                        ttsProxy.say("Hello android");
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        Log.e(TAG, "Error", e);
+                    }
+                }
+            });
+            connectThread.start();
+        }
+    }
+
+    boolean isNotBlank(String str) {
+        return (null != str) && !str.trim().equals("");
     }
 }
